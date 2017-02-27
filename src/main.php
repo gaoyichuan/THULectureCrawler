80ba6f4a3babe89486b51d1bf6347b4f4fd60d7c
gaoyichuan <gaoyichuan@eduno1.com>
2017-02-27 20:13:14 +0800

    Use main to generate ics

--- src/main.php
+++ src/main.php
@@ -8,12 +8,13 @@
 
 require_once 'crawler.php';
 require_once 'info.php';
+require_once '../vendor/autoload.php';
 
+$vCalendar = new \Eluceo\iCal\Component\Calendar('清华大学文化素质教育讲座');
+$timezone = new \DateTimeZone('Asia/Shanghai');
 $crawler = new Crawler("http://postinfo.tsinghua.edu.cn/f/jiaowugonggao/more", false);
 
 $lectures = file_exists('../data/lectures.json') ? json_decode(file_get_contents('../data/lectures.json'), true) : array();
-
-var_dump($lectures);
 
 $links = array();
 
@@ -40,3 +41,25 @@ foreach ($links as $link) {
 $fp = fopen('../data/lectures.json', 'w');
 fwrite($fp, json_encode($lectures));
 fclose($fp);
+
+foreach ($lectures as $lecture) {
+    $vEvent = new \Eluceo\iCal\Component\Event();
+
+    $datetime = new \DateTime($lecture['datetime'], $timezone);
+
+    $vEvent
+        ->setUseTimezone(true)
+        ->setSummary($lecture['title'])
+        ->setDtStart($datetime)
+        ->setDuration(new \DateInterval('PT2H'))
+        ->setNoTime(false)
+        ->setLocation($lecture['location'])
+        ->setDescription($lecture['speaker'])
+        ->setUrl($lecture['link']);
+
+    $vCalendar->addComponent($vEvent);
+}
+
+$cal = fopen('../data/cal.ics', 'w');
+fwrite($cal, $vCalendar->render());
+fclose($cal);
